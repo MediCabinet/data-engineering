4388a487bc4daf2091ae1d7d7d9831c2f6715937
Vanagand <michlaporte15@gmail.com>
2020-04-27 20:17:26 -0300

    web_app INIT

---
+++ web_app/__init__.py
@@ -0,0 +1,34 @@
+# app/__init__.py
+
+# import packages
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+# Import routes
+from app.models import db, migrate
+from app.routes.flask_app import flask_app
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # configure the database
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # configure routes
+    app.register_blueprint(flask_app)
+
+    return app
+
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
