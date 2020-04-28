d250557929ab71735f082e814640b52bb906e585
Lord-Kanzler <climberalex@gmail.com>
2020-04-27 22:55:05 -0400

    modified flask_app.py

--- app/routes/flask_app.py
+++ app/routes/flask_app.py
@@ -1,15 +1,17 @@
 # app/routes/flask_app.py
 
 # import packages
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 flask_app = Blueprint("flask_app", __name__)
 
 @flask_app.route("/")
 def home():
-    return f"Hello World!"
+    hello = "Hello World!"
+    return jsonify(hello)
 
 
 @flask_app.route("/about")
 def about():
-    return "About me"
+    return jsonify("About me")
+
